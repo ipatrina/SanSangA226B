de17604b6e2b2b5d86f6b7b05e035de790388b84
iPatrina <167176631+ipatrina@users.noreply.github.com>
2025-07-27 14:42:56 +0800

    Add files via upload

--- login.c
+++ login.c
@@ -23,7 +23,7 @@ void disable_echo(struct termios *old) {
     struct termios new;
     tcgetattr(STDIN_FILENO, old);
     new = *old;
-    new.c_lflag &= ~ECHO;
+    new.c_lflag &= ~(ECHO | ICANON);
     tcsetattr(STDIN_FILENO, TCSANOW, &new);
 }
 
@@ -34,14 +34,30 @@ void enable_echo(struct termios *old) {
 #define PASSWORD_HASH "7c9d79a9"
 
 int main() {
-    char input_password[33];
+    char input_password[33] = {0};
     char hash_output[9];
     struct termios old_term;
 
-    printf("Android     \n\nLogin: root\nPassword: ");
+    printf("Android     \n\nLogin: root        \nPassword: ");
     disable_echo(&old_term);
-    fgets(input_password, sizeof(input_password), stdin);
-    input_password[strcspn(input_password, "\n")] = 0;
+
+    int pos = 0;
+    int ch;
+    while ((ch = getchar()) != '\n' && ch != '\r') {
+        if (ch == 127 || ch == 8) {
+            if (pos > 0) {
+                pos--;
+                input_password[pos] = '\0';
+                printf("\b \b");
+                fflush(stdout);
+            }
+        } else if (pos < (int)(sizeof(input_password) - 1) && ch >= 32 && ch <= 126) {
+            input_password[pos++] = ch;
+            printf("*");
+            fflush(stdout);
+        }
+    }
+
     enable_echo(&old_term);
     printf("\n\n");
 
@@ -50,7 +66,7 @@ int main() {
 
     if (strcmp(hash_output, PASSWORD_HASH) == 0) {
         execl("/system/bin/sh", "sh", NULL);
-        printf("Accessing error.\n\n");
+        printf("Access error.\n\n");
         exit(EXIT_FAILURE);
     } else {
         printf("Access denied.\n\n");
